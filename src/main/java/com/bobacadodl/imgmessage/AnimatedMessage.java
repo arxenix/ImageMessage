2fe7dd3c29fa073f428b4c5f2f65596624a8de27
bobacadodl <ankursundara@msn.com>
2014-01-26 00:04:07 -0500

    Updated AnimatedMessage to use ImageInputStreams, to allow getResource() compatability

--- src/main/java/com/bobacadodl/imgmessage/AnimatedMessage.java
+++ src/main/java/com/bobacadodl/imgmessage/AnimatedMessage.java
@@ -5,6 +5,7 @@ import com.sun.imageio.plugins.gif.GIFImageReaderSpi;
 
 import javax.imageio.ImageIO;
 import javax.imageio.ImageReader;
+import javax.imageio.stream.ImageInputStream;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -25,17 +26,21 @@ public class AnimatedMessage {
     }
 
     public AnimatedMessage(File gifFile, int height, char imgChar) throws IOException {
-        List<BufferedImage> frames = getFrames(gifFile);
+        this(ImageIO.createImageInputStream(gifFile), height, imgChar);
+    }
+
+    public AnimatedMessage(ImageInputStream imageInputStream, int height, char imgChar) throws IOException {
+        List<BufferedImage> frames = getFrames(imageInputStream);
         images = new ImageMessage[frames.size()];
         for (int i = 0; i < frames.size(); i++) {
             images[i] = new ImageMessage(frames.get(i), height, imgChar);
         }
     }
 
-    public ArrayList<BufferedImage> getFrames(File gifFile) throws IOException {
+    public ArrayList<BufferedImage> getFrames(ImageInputStream imageInputStream) throws IOException {
         ArrayList<BufferedImage> frames = new ArrayList<BufferedImage>();
         ImageReader ir = new GIFImageReader(new GIFImageReaderSpi());
-        ir.setInput(ImageIO.createImageInputStream(gifFile));
+        ir.setInput(ImageIO.createImageInputStream(imageInputStream));
         for (int i = 0; i < ir.getNumImages(true); i++)
             frames.add(ir.read(i));
         return frames;
